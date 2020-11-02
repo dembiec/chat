670ffe57b6ef29d48a5b5d8a43fcb349d03666d2
Dominik Rembis <dominik.rembis@gmail.com>
2020-11-02 10:55:12 +0100

    Changes to the default files in the user model and user migration.

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
     ];
@@ -28,16 +29,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 }
