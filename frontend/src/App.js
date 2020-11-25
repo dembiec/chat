f4b5c88d69e2cac5642e1627cf67bb21753c8c59
Dominik Rembis <dominik.rembis@gmail.com>
2020-11-25 18:17:55 +0100

    A class has been created that is sending a request for logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Login from "./components/login";
 import Register from "./components/register";
+import Logout from "./components/logout";
+import Header from "./components/header";
 
 class App extends Component
 {
@@ -13,6 +15,10 @@ class App extends Component
             <div className="w-8/12 h-auto rounded-sm shadow-xl bg-white">
               <Route path="/login" component={Login} />
               <Route path="/register" component={Register} />
+              <Route path="/logout" component={Logout} />
+              <Route exact path="/">
+                <Header />
+              </Route>
             </div>
           </div>
         </Switch>
---
+++ frontend/src/components/logout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Redirect} from "react-router-dom";
+
+class Logout extends Component
+{
+  render() 
+  {
+    localStorage.clear();
+    return <Redirect to="/login" />;
+  }
+}
+
+export default Logout;
