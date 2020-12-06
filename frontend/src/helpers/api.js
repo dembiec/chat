8f7eff2dfa65045ee7a5520c5299c9472f23508b
Dominik Rembis <dominik.rembis@gmail.com>
2020-12-06 16:25:42 +0100

    Adding jwt token in request header fixed

--- frontend/src/helpers/api.js
+++ frontend/src/helpers/api.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
+let headers = {
+    'Content-Type': `application/json` 
+} 
+
+const location = window.location.pathname;
+if (location !== "/login" || location !== "/register") {
+    if (localStorage.token) {
+        headers.Authorization = `Bearer ${localStorage.token}`;
+    }
+}
+
 const Api = axios.create({
     baseURL: `http://127.0.0.1:8000/api`,
-    headers: {
-        'Content-Type': `application/json`,
-        'Authorization': localStorage.token ? `Bearer ${localStorage.token}` : null
-    }
+    headers
 });
 
 export default Api;
