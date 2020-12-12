ee30ba29b71491589da5f263ba05196c6ea9a373
Dominik Rembis <dominik.rembis@gmail.com>
2020-12-12 13:30:15 +0100

    Fixed a bug related to incorrect user randomization when there were not enough users in the database

--- api/app/Http/Controllers/Api/RandomUsersController.php
+++ api/app/Http/Controllers/Api/RandomUsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RandomUsersRequest AS RURequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\QueryException;
 
 class RandomUsersController extends Controller
@@ -12,8 +13,11 @@ class RandomUsersController extends Controller
     public function show(RURequest $request, $randomUsers = 1)
     {
         try {
-            $randomUsers = User::all()->random(htmlspecialchars($randomUsers));
-            return responder()->success($randomUsers)->respond();
+            $results = User::where('id', '<>', Auth::id())
+                ->inRandomOrder()
+                ->limit(htmlspecialchars($randomUsers))
+                ->get();
+            return responder()->success($results)->respond();
         } catch (QueryException $e) {
             return responder()
                 ->error()
