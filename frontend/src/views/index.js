bbc9a10b0dfec733d593cd93230badff0b5f3740
Dominik Rembis <dominik.rembis@gmail.com>
2021-01-20 12:33:55 +0100

    Improved responsiveness

--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -68,7 +68,7 @@ class Index extends Component
                     <div className="lg:w-1/2 md:w-1/4 h-full">
                         <div className="flex flex-row justify-start items-center h-full">
                             <button
-                                className="md:hidden sm:block w-auto h-6 focus:outline-none"
+                                className="lg:hidden md:block w-auto h-6 focus:outline-none"
                                 onClick={this.setStatus}
                             >
                                 <svg
@@ -79,7 +79,7 @@ class Index extends Component
                                     <path d="M57.633,129.007L165.93,237.268c4.752,4.74,12.451,4.74,17.215,0c4.752-4.74,4.752-12.439,0-17.179   l-99.707-99.671l99.695-99.671c4.752-4.74,4.752-12.439,0-17.191c-4.752-4.74-12.463-4.74-17.215,0L57.621,111.816   C52.942,116.507,52.942,124.327,57.633,129.007z"/>
                                 </svg>
                             </button>
-                            <div className="md:flex sm:hidden flex-row space-x-2 items-center">
+                            <div className="lg:flex md:hidden flex-row space-x-2 items-center">
                                 <span className="rounded-full h-3 w-3 bg-red-500"></span>
                                 <span className="rounded-full h-3 w-3 bg-yellow-400"></span>
                                 <span className="rounded-full h-3 w-3 bg-green-400"></span>
