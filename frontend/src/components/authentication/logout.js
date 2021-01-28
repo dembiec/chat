b6d46acb86eb600d666fecee90a1e2d15ee21b9d
Dominik Rembis <dominik.rembis@gmail.com>
2021-01-28 12:26:25 +0100

    Improved logout methods

--- frontend/src/components/authentication/logout.js
+++ frontend/src/components/authentication/logout.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import Api, {JWTtoken} from "../../helpers/api";
 import {Redirect} from "react-router-dom";
 
 class Logout extends Component
 {
+  constructor(props)
+  {
+    super(props);
+
+    JWTtoken(sessionStorage.getItem("token"));
+    Api.get("/logout");
+    sessionStorage.clear();
+  }
+  
   render() 
   {
-    sessionStorage.clear();
     return <Redirect to="/login" />;
   }
 }
